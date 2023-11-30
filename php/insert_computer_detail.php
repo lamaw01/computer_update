95b2cfe93205b1d698caaa73adcef9ddf9517cd5
lamaw01 <Administrator@MIS-JANREY.uc-1.domain>
2023-12-01 05:13:49 +0800

    add ms office api

--- php/insert_computer_detail.php
+++ php/insert_computer_detail.php
@@ -23,43 +23,54 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
     $last_id_sql = 'SELECT * FROM tbl_update ORDER BY id DESC LIMIT 1;';
 
-    // query insert new machine details
     $insert_sql = 'INSERT INTO tbl_computer_details(uuid,hostname,os,defender,cpu,motherboard,ram,storage,user,network,monitor,browser,msoffice,update_id)
     VALUES (:uuid,:hostname,:os,:defender,:cpu,:motherboard,:ram,:storage,:user,:network,:monitor,:browser,:msoffice,:update_id)';
+
+    $update_hostname_sql = 'UPDATE tbl_computer_details SET hostname=:hostname WHERE uuid=:uuid';
+
+    $latest_hostname_sql = 'SELECT hostname FROM tbl_computer_details WHERE uuid=:uuid ORDER BY id DESC LIMIT 1;';
 
     try {
         // $set=$conn->prepare("SET SQL_MODE=''");
         // $set->execute();
 
-        // $get_last_id = $conn->prepare($last_id_sql);
-        // $get_last_id->execute();
-        // $result_get_last_id = $get_last_id->fetch(PDO::FETCH_ASSOC);
-        // $update_id = $result_get_last_id["id"]
-        // echo json_encode($update_id);
+        $get_latest_hostname_sql = $conn->prepare($latest_hostname_sql);
+        $get_latest_hostname_sql->bindParam(':uuid', $uuid, PDO::PARAM_STR);
+        $get_latest_hostname_sql->execute();
+        $result_get_latest_hostname_sql = $get_latest_hostname_sql->fetch(PDO::FETCH_ASSOC);
+        $latest_hostname = $result_get_latest_hostname_sql["hostname"];
+        // echo json_encode($latest_hostname);
 
-        $get_sql = $conn->prepare($last_id_sql);
-        $get_sql->execute();
-        $result_get_sql = $get_sql->fetch(PDO::FETCH_ASSOC);
-        $update_id = $result_get_sql["id"];
-        // echo json_encode($result_get_sql["id"]);
+        if($latest_hostname != $hostname){
+            $sql_update = $conn->prepare($update_hostname_sql);
+            $sql_update->bindParam(':hostname', $hostname, PDO::PARAM_STR);
+            $sql_update->bindParam(':uuid', $uuid, PDO::PARAM_STR);
+            $sql_update->execute();
+            echo json_encode(array('success'=>true,'message'=>'update','latest_hostname'=>$latest_hostname));
+        }else{
+            $get_sql = $conn->prepare($last_id_sql);
+            $get_sql->execute();
+            $result_get_sql = $get_sql->fetch(PDO::FETCH_ASSOC);
+            $update_id = $result_get_sql["id"];
 
-        $sql_insert = $conn->prepare($insert_sql);
-        $sql_insert->bindParam(':uuid', $uuid, PDO::PARAM_STR);
-        $sql_insert->bindParam(':hostname', $hostname, PDO::PARAM_STR);
-        $sql_insert->bindParam(':os', $os, PDO::PARAM_STR);
-        $sql_insert->bindParam(':defender', $defender, PDO::PARAM_STR);
-        $sql_insert->bindParam(':cpu', $cpu, PDO::PARAM_STR);
-        $sql_insert->bindParam(':motherboard', $motherboard, PDO::PARAM_STR);
-        $sql_insert->bindParam(':ram', $ram, PDO::PARAM_STR);
-        $sql_insert->bindParam(':storage', $storage, PDO::PARAM_STR);
-        $sql_insert->bindParam(':user', $user, PDO::PARAM_STR);
-        $sql_insert->bindParam(':network', $network, PDO::PARAM_STR);
-        $sql_insert->bindParam(':monitor', $monitor, PDO::PARAM_STR);
-        $sql_insert->bindParam(':browser', $browser, PDO::PARAM_STR);
-        $sql_insert->bindParam(':msoffice', $msoffice, PDO::PARAM_STR);
-        $sql_insert->bindParam(':update_id', $update_id, PDO::PARAM_INT);
-        $sql_insert->execute();
-        echo json_encode(array('success'=>true,'message'=>'insert'));
+            $sql_insert = $conn->prepare($insert_sql);
+            $sql_insert->bindParam(':uuid', $uuid, PDO::PARAM_STR);
+            $sql_insert->bindParam(':hostname', $hostname, PDO::PARAM_STR);
+            $sql_insert->bindParam(':os', $os, PDO::PARAM_STR);
+            $sql_insert->bindParam(':defender', $defender, PDO::PARAM_STR);
+            $sql_insert->bindParam(':cpu', $cpu, PDO::PARAM_STR);
+            $sql_insert->bindParam(':motherboard', $motherboard, PDO::PARAM_STR);
+            $sql_insert->bindParam(':ram', $ram, PDO::PARAM_STR);
+            $sql_insert->bindParam(':storage', $storage, PDO::PARAM_STR);
+            $sql_insert->bindParam(':user', $user, PDO::PARAM_STR);
+            $sql_insert->bindParam(':network', $network, PDO::PARAM_STR);
+            $sql_insert->bindParam(':monitor', $monitor, PDO::PARAM_STR);
+            $sql_insert->bindParam(':browser', $browser, PDO::PARAM_STR);
+            $sql_insert->bindParam(':msoffice', $msoffice, PDO::PARAM_STR);
+            $sql_insert->bindParam(':update_id', $update_id, PDO::PARAM_INT);
+            $sql_insert->execute();
+            echo json_encode(array('success'=>true,'message'=>'insert'));
+        }
     } catch (PDOException $e) {
         echo json_encode(array('success'=>false,'message'=>$e->getMessage()));
     } finally{
