b593c3ad68a6173e918b3268e5b7edaaccf5bdaf
lamaw01 <Administrator@MIS-JANREY.uc-1.domain>
2024-02-21 14:05:15 +0800

    added try catch all

--- scripts/script_win10.ps1
+++ scripts/script_win10.ps1
@@ -3,58 +3,170 @@ $header = @{
     "Content-Type"="application/json; charset=UTF-8"
 }
 
-# check if we update code and if we get computer detail
-$update_status = Invoke-RestMethod -Uri "http://103.62.153.74:53000/computer_detail/get_update_latest.php" -Method 'Get' -Headers $header
+$update_code = 0
+$status = 0
 
-$update_code = $update_status | Select-Object -ExpandProperty "update_code"
+try {
+    # check if we update code and if we get computer detail
+    $update_status = Invoke-RestMethod -ErrorAction Stop -Uri "http://103.62.153.74:53000/computer_detail/get_update_latest.php" -Method 'Get' -Headers $header
+    $update_code = $update_status | Select-Object -ExpandProperty "update_code"
+    $status = $update_status | Select-Object -ExpandProperty "status"
+}
+catch {
+    Write-Host 'Error update_status'
+    Write-Host $_
+}
 
 # update code script
 if($update_code -eq 1){
-    Invoke-WebRequest -Uri 'http://103.62.153.74:53000/computer_detail/script_win10.ps1' -OutFile 'C:/script_win10.ps1'
+    try {
+        Invoke-WebRequest -ErrorAction Stop -Uri 'http://103.62.153.74:53000/computer_detail/script_win10.ps1' -OutFile 'C:/script_win10.ps1'
+    }
+    catch {
+        Write-Host 'Error update_code'
+        Write-Host $_
+    }   
 }
-
-$status = $update_status | Select-Object -ExpandProperty "status"
 
 # get computer detail
 if($status -eq 1){
 
-    $uuid = (Get-WmiObject -Class Win32_ComputerSystemProduct).UUID
+    $uuid = ""
+    $hostname = ""
+    $network = ""
+    $os = ""
+    $defender = ""
+    $cpu = ""
+    $gpu = ""
+    $motherboard = ""
+    $ram = ""
+    $storage = ""
+    $user = ""
+    $monitor = ""
+    $browser = ""
+    $msoffice = ""
 
-    $hostname = (Get-CimInstance Win32_ComputerSystem).Name
+    try {
+        $uuid = (Get-WmiObject -Class Win32_ComputerSystemProduct -ErrorAction Stop).UUID
+    }
+    catch {
+        Write-Host 'Error getting uuid'
+        Write-Host $_
+    }
 
-    $network = (Get-WmiObject Win32_NetworkAdapterConfiguration -Filter 'IPEnabled=TRUE' | Select-Object -ExpandProperty IPAddress | Where-Object { $_ -match '(\d{1,3}\.){3}\d{1,3}' }).Trim()
+    try {
+        $hostname = (Get-CimInstance Win32_ComputerSystem -ErrorAction Stop).Name
+    }
+    catch {
+        Write-Host 'Error getting hostname'
+        Write-Host $_
+    }
 
-    $os = (Get-CimInstance Win32_OperatingSystem | Format-List Caption, BuildNumber, Version, SystemDirectory | Out-String).Trim()
+    try {
+        $network = (Get-WmiObject Win32_NetworkAdapterConfiguration -Filter 'IPEnabled=TRUE' -ErrorAction Stop | Select-Object -ExpandProperty IPAddress | Where-Object { $_ -match '(\d{1,3}\.){3}\d{1,3}' }).Trim()
+    }
+    catch {
+        Write-Host 'Error getting network'
+        Write-Host $_
+    }
 
-    $defender = (Get-MpComputerStatus | Format-List AntivirusSignatureVersion, QuickScanSignatureVersion | Out-String).Trim()
+    try {
+        $os = (Get-CimInstance Win32_OperatingSystem -ErrorAction Stop | Format-List Caption, BuildNumber, Version, SystemDirectory | Out-String).Trim()
+    }
+    catch {
+        Write-Host 'Error getting os'
+        Write-Host $_
+    }
 
-    $cpu = (Get-CimInstance -Class Win32_Processor | Format-List Name, @{n="ClockSpeed (Ghz)"; e={[math]::Round(($_.CurrentClockSpeed/1000),2)}}, NumberOfCores, NumberOfLogicalProcessors, SerialNumber | Out-String).Trim()
+    try {
+        $defender = (Get-MpComputerStatus -ErrorAction Stop | Format-List AntivirusSignatureVersion, QuickScanSignatureVersion | Out-String).Trim()
+    }
+    catch {
+        Write-Host 'Error getting defender'
+        Write-Host $_
+    }
 
-    $gpu = (Get-WmiObject Win32_VideoController | Format-List DeviceID, Name, VideoProcessor, @{n="AdapterRAM (GB)"; e={[math]::Round(($_.AdapterRAM/1GB),2)}} | Out-String).Trim()
+    try {
+        $cpu = (Get-CimInstance -Class Win32_Processor -ErrorAction Stop | Format-List Name, @{n="ClockSpeed (Ghz)"; e={[math]::Round(($_.CurrentClockSpeed/1000),2)}}, NumberOfCores, NumberOfLogicalProcessors, SerialNumber | Out-String).Trim()
+    }
+    catch {
+        Write-Host 'Error getting cpu'
+        Write-Host $_
+    }
 
-    $motherboard = (Get-CimInstance -Class Win32_BaseBoard | Format-List Manufacturer, Product, SerialNumber, Version | Out-String).Trim()
+    try {
+        $gpu = (Get-WmiObject Win32_VideoController -ErrorAction Stop | Format-List DeviceID, Name, VideoProcessor, @{n="AdapterRAM (GB)"; e={[math]::Round(($_.AdapterRAM/1GB),2)}} | Out-String).Trim()
+    }
+    catch {
+        Write-Host 'Error getting gpu'
+        Write-Host $_
+    }
 
-    $ram = (Get-CimInstance Win32_PhysicalMemory | Format-List Manufacturer, SerialNumber, DeviceLocator, @{n="Size (GB)"; e={($_.Capacity/1GB)}}, @{n="ClockSpeed (MHz)"; e={($_.ConfiguredClockSpeed)}} | Out-String).Trim()
+    try {
+        $motherboard = (Get-CimInstance -Class Win32_BaseBoard -ErrorAction Stop | Format-List Manufacturer, Product, SerialNumber, Version | Out-String).Trim()
+    }
+    catch {
+        Write-Host 'Error getting motherboard'
+        Write-Host $_
+    }
 
-    # $storage = (Get-WMIObject Win32_DiskDrive | Format-List Name, Model, SerialNumber, @{n="Size (GB)"; e={[math]::Round(($_.Size/1GB),2)}} | Out-String).Trim()
+    try {
+        $ram = (Get-CimInstance Win32_PhysicalMemory -ErrorAction Stop | Format-List Manufacturer, SerialNumber, DeviceLocator, @{n="Size (GB)"; e={($_.Capacity/1GB)}}, @{n="ClockSpeed (MHz)"; e={($_.ConfiguredClockSpeed)}} | Out-String).Trim()
+    }
+    catch {
+        Write-Host 'Error getting ram'
+        Write-Host $_
+    }
 
-    $storage = (Get-PhysicalDisk | Format-List FriendlyName, SerialNumber, @{n="Size (GB)"; e={[math]::Round(($_.Size/1GB),2)}}, MediaType, HealthStatus | Out-String).Trim()
+    try {
+        # $storage = (Get-WMIObject Win32_DiskDrive | Format-List Name, Model, SerialNumber, @{n="Size (GB)"; e={[math]::Round(($_.Size/1GB),2)}} | Out-String).Trim()
 
-    $user = (Get-CimInstance Win32_ComputerSystem | Format-List  Name, Username, Domain | Out-String).Trim()
+        $storage = (Get-PhysicalDisk -ErrorAction Stop | Format-List FriendlyName, SerialNumber, @{n="Size (GB)"; e={[math]::Round(($_.Size/1GB),2)}}, MediaType, HealthStatus | Out-String).Trim()
+    }
+    catch {
+        Write-Host 'Error getting storage'
+        Write-Host $_
+    }
 
-    $msoffice = ((Get-Item 'C:\Program Files (x86)\Microsoft Office\Office14\WINWORD.exe').VersionInfo | Format-List ProductVersion | Out-String).Trim()
+    try {
+        $user = (Get-CimInstance Win32_ComputerSystem -ErrorAction Stop | Format-List  Name, Username, Domain | Out-String).Trim()
+    }
+    catch {
+        Write-Host 'Error getting user'
+        Write-Host $_
+    }
 
-    $monitor = (Get-WmiObject WmiMonitorID -Namespace root\wmi | Format-List -Property @(
-        @{Name = 'Manufacturer'; Expression = {[string]::new([char[]]($_.Manufacturername)).Trim("`0")}}
-        @{Name = 'Model'; Expression = {[string]::new([char[]]($_.UserFriendlyName)).Trim("`0")}}
-        @{Name = 'Serial'; Expression = {[string]::new([char[]]($_.SerialNumberID)).Trim("`0")}}
-    ) | Out-String).Trim()
+    try {
+        $msoffice = ((Get-Item 'C:\Program Files (x86)\Microsoft Office\Office14\WINWORD.exe' -ErrorAction Stop).VersionInfo | Format-List ProductVersion | Out-String).Trim()
+    }
+    catch {
+        Write-Host 'Error getting msoffice'
+        Write-Host $_
+    }
 
-    $browser =
-    (@(
-        [pscustomobject]@{Chrome=(Get-Item (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').'(Default)').VersionInfo.ProductVersion},
-        [pscustomobject]@{MSEdge=(Get-Item (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe').'(Default)').VersionInfo.ProductVersion}
-    ) | Format-List | Out-String).Trim()
+    try {
+        $monitor = (Get-WmiObject WmiMonitorID -Namespace root\wmi -ErrorAction Stop | Format-List -Property @(
+            @{Name = 'Manufacturer'; Expression = {[string]::new([char[]]($_.Manufacturername)).Trim("`0")}}
+            @{Name = 'Model'; Expression = {[string]::new([char[]]($_.UserFriendlyName)).Trim("`0")}}
+            @{Name = 'Serial'; Expression = {[string]::new([char[]]($_.SerialNumberID)).Trim("`0")}}
+        ) | Out-String).Trim()
+    }
+    catch {
+        Write-Host 'Error getting monitor'
+        Write-Host $_
+    }
+   
+    try {
+        $browser =
+        (@(
+            [pscustomobject]@{Chrome=(Get-Item (Get-ItemProperty -ErrorAction Stop 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').'(Default)').VersionInfo.ProductVersion},
+            [pscustomobject]@{MSEdge=(Get-Item (Get-ItemProperty -ErrorAction Stop 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\msedge.exe').'(Default)').VersionInfo.ProductVersion}
+        ) | Format-List | Out-String).Trim()
+    }
+    catch {
+        Write-Host 'Error getting browser'
+        Write-Host $_
+    }
 
     $body = @{
     "uuid"="$uuid"
@@ -73,5 +185,13 @@ if($status -eq 1){
     "msoffice"="$msoffice"
     } | ConvertTo-Json
 
-    Invoke-RestMethod -Uri "http://103.62.153.74:53000/computer_detail/insert_computer_detail.php" -Method 'Post' -Body $body -Headers $header | ConvertTo-Json
+    # Write-Host $body
+
+    try {
+        Invoke-RestMethod -ErrorAction Stop -Uri "http://103.62.153.74:53000/computer_detail/insert_computer_detail.php" -Method 'Post' -Body $body -Headers $header | ConvertTo-Json
+    }
+    catch {
+        Write-Host 'Error inserting data'
+        Write-Host $_
+    }
 }
