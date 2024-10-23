5876da6a6914b3da4ffc07762c31cd61b2d5f272
lamaw01 <Administrator@MIS-JANREY.uc-1.domain>
2024-10-23 11:28:05 +0800

    update ssl error

--- scripts/setup_win10.bat
+++ scripts/setup_win10.bat
@@ -3,7 +3,7 @@
 powershell -c Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force 
 powershell Get-ExecutionPolicy
 
-powershell -c "Invoke-WebRequest -Uri 'http://103.62.153.74:53000/computer_detail/script_win10.ps1' -OutFile 'C:/script_win10.ps1'"
+powershell -c "Invoke-WebRequest -Uri 'https://konek.parasat.tv:53000/computer_detail/script_win10.ps1' -OutFile 'C:/script_win10.ps1'"
 
 schtasks /CREATE /SC DAILY /RL highest /RU system /TN "computer_detail" /TR "powershell -ExecutionPolicy Bypass C:/script_win10.ps1" /ST 09:00 /RI 60 /DU 24:00
 
--- scripts/setup_win7.bat
+++ scripts/setup_win7.bat
@@ -3,7 +3,7 @@
 powershell -c Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force 
 powershell Get-ExecutionPolicy
 
-powershell -c "(new-object System.Net.WebClient).DownloadFile('http://103.62.153.74:53000/computer_detail/script_win7.ps1','C:/script_win7.ps1')"
+powershell -c "(new-object System.Net.WebClient).DownloadFile('https://konek.parasat.tv:53000/computer_detail/script_win7.ps1','C:/script_win7.ps1')"
 
 schtasks /CREATE /SC DAILY /RL highest /RU system /TN "computer_detail" /TR "powershell -ExecutionPolicy Bypass C:/script_win7.ps1" /ST 09:00 /RI 60 /DU 24:00
 
